5b77528ec13cb0ddae17d35a9eb63e92710e2bdc
Ulaş Noyan <99445511+noyanulas@users.noreply.github.com>
2022-08-21 02:27:00 +0300

    Add files via upload
    I was using one of my old solutions as a base so I couldn't change the name of the folder, won't happen again.

---
+++ UlasNoyan_Homework1/Controllers/StaffController.cs
@@ -0,0 +1,64 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace UlasNoyan_Homework2
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        /*
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public double Salary { get; set; }
+    
+        */
+
+
+
+
+
+
+        //Liste mekaniğini implement edemedim sanırım yazış stilimde problem var haftaki ödevde öğrenmiş ve düzeltmiş olma umuduyla...
+
+
+
+
+
+
+
+
+
+        [HttpGet("get staff information")]
+        public List<Staff> get()
+        {
+            List<Staff> staffs = new List<Staff>();
+            return new List<Staff>();
+        }
+        [HttpPut]
+        public void Put([FromBody] Staff a)
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete([FromBody] Int32 Id)
+        {
+
+        }
+
+
+
+        [HttpPost("Get Staff Information")]
+        public void Post([FromBody] Staff a)
+        {
+      
+        }
+    }
+}
+
---
+++ UlasNoyan_Homework1/Staff.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UlasNoyan_Homework2
+{
+    public class Staff  
+    {
+        //list of properties of the "Staff" class
+        [Required(ErrorMessage = "Id is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(120, MinimumLength = 20, ErrorMessage = "First name must be between 20 and 140 characters in length")]
+        public string Name { get; set; }
+
+        [StringLength(120, MinimumLength = 20, ErrorMessage = "Last name must be between 20 and 140 characters in length")]
+        [Required(ErrorMessage = "Last name is required")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
+        [DataType(DataType.DateTime)]
+        [Range(typeof(DateTime), "11/11/1945", "10/10/2002",
+        ErrorMessage = "Value for {0} must be between {1} and {2}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Range(+900000000000, +910000000000)]
+        public Int64 PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Salary is required")]
+        [Range(2000,9000)]
+        public double Salary { get; set; }
+    }
+}
